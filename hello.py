f63d6c1b6bcf49a43031288a96a2a056d51a7497
hyd <hydfreeman@163.com>
2019-03-06 15:57:04 +0800

    add file hello.py

---
+++ hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print('hello!! world')
+name = input('please input your name:')
+print('hello',name,'!!!')
+print('\'output:\'')
+print('\n','\\\t\\')
+
+
+
+
+
+
+
